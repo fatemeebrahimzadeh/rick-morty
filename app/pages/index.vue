570b6d29396a0748d36681569777dd32aa8a9637
fatemeebrahimzadeh <f.ebrahimzadeh@alibaba.ir>
2026-02-03 00:33:45 +0330

    chore: wire up tailwind

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-900">
+    <h1 class="text-3xl font-bold text-green-400">
+      Rick & Morty
+    </h1>
+  </div>
+</template>
